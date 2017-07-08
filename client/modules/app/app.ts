a8c3105d2c357353da0b17199f6ecac993442816
Herman-Scheer <g@herman-scheer.com>
2017-07-08 07:12:31 -0700

    rect

--- client/modules/app/app.ts
+++ client/modules/app/app.ts
@@ -1,14 +1,16 @@
-import * as P from 'p5'
+import * as P5 from 'p5'
 
 const sketch = (p) => {
   p.setup = () => {
-    p.createCanvas(400, 400)
+    p.createCanvas(640, 360)
   }
 
   p.draw = () => {
-    p.background(255, 0, 0)
-    p.ellipse(200, 200, 50, 50)
+    // x, y, w, h
+    // p.background(255, 0, 0)
+    // p.ellipse(200, 200, 50, 50)
+    p.rect(100, 200, 75, 125)
   }
 }
 
-export const Sketch = new P(sketch)
+export const Sketch = new P5(sketch)
